3bbff4941a64ba749122b0b143200b84167c2660
thirtydegreelaw-svg <thirtydegreelaw@gmail.com>
2026-08-01 02:59:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("Origin Web3 Loaded Successfully");
+
+console.log("Origin Web3 Loaded Successfully");
+
+const button=document.querySelector(".btn-primary");
+
+if(button){
+
+button.addEventListener("click",function(){
+
+alert("Welcome to Origin Web3 🚀");
+
+});
+
+}
+
 });
